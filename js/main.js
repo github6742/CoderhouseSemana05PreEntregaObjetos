c35aa957ba15887a75dcdcde368e1dc92f26671e
Sergio Rodriguez <srodriguezlab@yahoo.com>
2026-09-22 23:09:59 -0300

    Se elimino del menu la llamada a carga inicial, y se ejecuta al principio del simulador, ademas se agrego el parseo del costo

--- js/main.js
+++ js/main.js
@@ -1,40 +1,24 @@
+// 22/09/2026
+// se eliminaron los archivos js que estaban de mas, fui haciendo pruebas y los deje en el proyecto por erro
+// se eliminaron los this fuera del constructor,
+// utilizo dos clases un a de archivos y la otra de modulo de inventario con un array de articulos
+// en esta clase realice la carga con valores harcodeados, porque de hacerlo por prompt, se hacia muy largo
+// directamente desarrolle la funcion de carga inicial que crea los articulos y los carga en el array de modulo de inventario
+// agregue la funcion de crear articulo desde el prompt
+// en el ingreso del costo faltaba el parseo del numero
 class Articulo {
       constructor(codigo, 
                   nombre, 
-                  unidad, 
-                  estado, 
-                  categoria, 
-                  subCategoria1,
-                  subCategoria2,
-                  subCategoria3,
-                  subCategoria4,
-                  linea, 
-                  grupo, 
-                  deCompra, 
-                  deVenta, 
-                  deInventario, 
-                  costoPesos,                   
-                  costoUsd){
+                  marca,
+                  costoPesos){
         this.codigo = codigo;
         this.nombre = nombre;
-        this.unidad = unidad;
-        this.estado = estado;    //activo, inactivo
-        this.categoria = categoria;  
-        this.subCategoria1 = subCategoria1;
-        this.subCategoria2 = subCategoria2;
-        this.subCategoria3 = subCategoria3;
-        this.subCategoria4 = subCategoria4;
-        this.lineaArticulo = linea;  
-        this.grupoArticulo = grupo;
-        this.deCompra = deCompra; // si/no
-        this.deVenta = deVenta; // si/no
-        this.deInventario = deInventario; // si/no
+        this.marca = marca;
         this.costoPesos = costoPesos;
-        this.costoUsd = costoUsd;
       };
 
-      modificaCostoPesosPorcentaje(porcentaje){
-        costoPesos += (costoPesos*(porcentaje/100));
+      modificarCostoPesosPorcentaje(porcentaje){
+        this.costoPesos += (this.costoPesos*(porcentaje/100));
       };
       
 };
@@ -42,111 +26,61 @@ class Articulo {
 //CLASE DE MODULO DE INVENTARIO
 class ModuloInventario{
     constructor(){     
-       this.maestroArticulos = [];    
+       this.maestroArticulos = [];
     };
 
     cargaInicial(){
 
-        if (maestroArticulos.length == 0) {
         const articulo1 = new Articulo("B240000",//CODIGO
                                        "TELEVISOR SMART TV PHILCO 33 PULGADAS", //NOMBRE
-                                       "UN",//UNIDAD
-                                       "A",//ESTADO
-                                       "TV Y AUDIO", // CATEGORIA 
-                                       "TELEVISOR", // SUBCATEGORIA1
-                                       "PULGADAS-00-33", // SUBCATEGORIA2
-                                       "PHILCO", //SUBCATEGORIA3
-                                       "PCOCB240033", //SUBCATEGORIA 4
-                                       "", //LINEA ARTICULO
-                                       "", //GRUPO ARTICULO
-                                       "Y",//DE COMPRA
-                                       "Y", // DE VENTA
-                                       "Y", // DE INVENTARIO
-                                        30000, //COSTO PESOS
-                                        300); //costo usd
+                                       "PHILCO",
+                                       30000); 
 
                                         
         const articulo2 = new Articulo("B250000",//CODIGO
                                        "TELEVISOR SMART TV PHILLIPS 33 PULGADAS", //NOMBRE
-                                       "UN",//UNIDAD
-                                       "A",//ESTADO
-                                       "TV Y AUDIO", // CATEGORIA 
-                                       "TELEVISOR", // SUBCATEGORIA1
-                                       "PULGADAS-00-33", // SUBCATEGORIA2
-                                       "PHILLIPOS", //SUBCATEGORIA3
-                                       "PHICB250033", //SUBCATEGORIA 4
-                                       "", //LINEA ARTICULO
-                                       "", //GRUPO ARTICULO
-                                       "Y",//DE COMPRA
-                                       "Y", // DE VENTA
-                                       "Y", // DE INVENTARIO
-                                        150000, //COSTO PESOS
-                                        1500); //costo usd
-
+                                       "PHILLIPS",
+                                       150000);
 
         const articulo3 = new Articulo("B260000",//CODIGO
                                        "TELEVISOR SMART TV SAMSUNG 33 PULGADAS", //NOMBRE
-                                       "UN",//UNIDAD
-                                       "A",//ESTADO
-                                       "TV Y AUDIO", // CATEGORIA 
-                                       "TELEVISOR", // SUBCATEGORIA1
-                                       "PULGADAS-00-33", // SUBCATEGORIA2
-                                       "SAMSUNG", //SUBCATEGORIA3
-                                       "SAMCB260033", //SUBCATEGORIA 4
-                                       "", //LINEA ARTICULO
-                                       "", //GRUPO ARTICULO
-                                       "Y",//DE COMPRA
-                                       "Y", // DE VENTA
-                                       "Y", // DE INVENTARIO
-                                        450000, //COSTO PESOS
-                                        300); //costo usd      
-                                        
+                                       "SAMSUNG",
+                                       450000); 
 
         const articulo4 = new Articulo("B270000",//CODIGO
                                        "TELEVISOR SMART TV JVL 33 PULGADAS", //NOMBRE
-                                       "UN",//UNIDAD
-                                       "A",//ESTADO
-                                       "TV Y AUDIO", // CATEGORIA 
-                                       "TELEVISOR", // SUBCATEGORIA1
-                                       "PULGADAS-00-33", // SUBCATEGORIA2
-                                       "JVL", //SUBCATEGORIA3
-                                       "JVLCB240033", //SUBCATEGORIA 4
-                                       "", //LINEA ARTICULO
-                                       "", //GRUPO ARTICULO
-                                       "Y",//DE COMPRA
-                                       "Y", // DE VENTA
-                                       "Y", // DE INVENTARIO
-                                        60000, //COSTO PESOS
-                                        400); //costo usd                                        
+                                       "JVL",
+                                       60000);                                     
 
-        maestroArticulos.push(articulo1);
-        maestroArticulos.push(articulo2);
-        maestroArticulos.push(articulo3);
-        maestroArticulos.push(articulo4);
-    } else {
-        console.log("Ya se realizo la carga inicial de articulos");
-        alert("Ya se realizo la carga inicial de articulos");
-    };
+        this.maestroArticulos.push(articulo1);
+        this.maestroArticulos.push(articulo2);
+        this.maestroArticulos.push(articulo3);
+        this.maestroArticulos.push(articulo4);
+
     };
 
     informarListaArticulos(){
+      
         let mensaje = "Listado de Articulos: \n";
         
         for ( const articulo of this.maestroArticulos) {
-            mensaje += articulo.codigo + '-' + articulo.nombre + ' - Costo: ' +  articulo.costoPesos + "\n";
+            mensaje += " - Codigo: " +  articulo.codigo + 
+                       " - Nombre: " +  articulo.nombre + 
+                       " - Marca: " +  articulo.marca  +    
+                       " - Costo: " +  articulo.costoPesos + "\n";
         };
         console.log(mensaje);
         alert(mensaje);
     };
 
-    modificaCostoArticulos(){
+    modificarCostoArticulos(){
         let porcentaje;
         do {
             porcentaje = prompt("Ingrese porcentaje");
         } while (!validarPorcentaje(porcentaje));
         
         for ( const articulo of this.maestroArticulos) {
-            articulo.modificaCostoPesosPorcentaje(porcentaje);            
+            articulo.modificarCostoPesosPorcentaje(parseFloat(porcentaje));            
         };
     };
 
@@ -160,6 +94,7 @@ function functionPrincipalSimulador(){
   console.log("LOG - ----------------------------------"); 
 
   const moduloInventario = new ModuloInventario();
+  moduloInventario.cargaInicial();
 
   let salir = false;
 
@@ -170,13 +105,13 @@ function functionPrincipalSimulador(){
                  alert("Debe ingresar un valor, vuelva a ingresar la opcion");
                  break;
             case 1:
-                 moduloInventario.cargaInicial();
+                 moduloInventario.maestroArticulos.push(cargarArticulo());
                  break;
             case 2:
                  moduloInventario.informarListaArticulos();
                  break;   
             case 3:
-                 moduloInventario.modificaCostoArticulos();
+                 moduloInventario.modificarCostoArticulos();
                  break;   
             case 9:
                  salir = true;
@@ -205,11 +140,12 @@ function seleccionarOpcionMenu(){
  
   let opcionMenu = prompt("MODULO DE INVENTARIO\n\n" + 
                           "Seleccione opcion:\n" + 
-                          " 1-Carga Inicial\n" +
+                          " 1-Crea Articulos\n" +
                           " 2-Lista de Articulos\n" +
                           " 3-Modifica Costo Articulos\n" +
                           " 9-Salida"
                          );
+
   console.log("LOG - seleccionarOpcionMenu - 0010 - opcionMenu = " + opcionMenu);
   
   if (esNulo(opcionMenu)){
@@ -219,6 +155,80 @@ function seleccionarOpcionMenu(){
   console.log("LOG - seleccionarOpcionMenu - 9999");
 
   return parseInt(opcionMenu);     
+};
+
+function cargarArticulo(){
+    let codigo;
+    do {
+        codigo = prompt("Ingrese codigo de articulo: ");
+    } while (!validarCodigo(codigo));
+
+    let nombre;
+    do {
+        nombre = prompt("Ingrese nombre de articulo: ");
+    } while (!validarNombre(nombre));
+
+    let marca;
+    do {
+         marca = prompt("Ingrese marca de articulo: ");
+    } while (!validarMarca(marca));
+
+    let costo;
+    do {
+         costo = prompt("Ingrese costo de articulo: ");
+    } while (!validarCosto(costo));
+
+    const articuloNuevo = new Articulo(codigo.toUpperCase(), nombre.toUpperCase(), marca.toUpperCase(), parseFloat(costo));
+
+    return articuloNuevo;
+
+};
+
+function validarCodigo(codigo){
+
+     if (esNulo(codigo)) {
+        alert("(Validar Codigo) - Debe Ingresar un Valor");                    
+        return false;
+      } else {
+        return true;
+     };
+
+};
+
+function validarNombre(nombre){
+
+     if (esNulo(nombre)) {
+        alert("(Validar Nombre) - Debe Ingresar un Valor");                    
+        return false;
+      } else {
+        return true;
+     };
+
+};
+
+function validarMarca(marca){
+
+     if (esNulo(marca)) {
+        alert("(Validar Marca) - Debe Ingresar un Valor");                    
+        return false;
+      } else {
+        return true;
+     };
+
+};
+
+function validarCosto(costo){
+
+     if (esNulo(costo)) {
+        alert("(Validar Costo) - Debe Ingresar un Valor");                    
+        return false;
+      } else if (esString(costo)) { 
+        alert("(Validar Costo) - Debe Ingresar un Valor Numerico");                    
+        return false;
+      } else {
+        return true;
+     };
+
 };
 
 
